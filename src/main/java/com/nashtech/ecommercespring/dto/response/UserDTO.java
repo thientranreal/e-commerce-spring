1d6f9d62ea4f079ba18db1092c48f438b75f6582
thientranreal <thientt2612@gmail.com>
2025-04-14 10:23:02 +0700

    update filed for UserDTO

--- src/main/java/com/nashtech/ecommercespring/dto/response/UserDTO.java
+++ src/main/java/com/nashtech/ecommercespring/dto/response/UserDTO.java
@@ -4,6 +4,7 @@ import com.nashtech.ecommercespring.model.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
@@ -13,6 +14,12 @@ public class UserDTO {
     private UUID id;
 
     private String email;
+
+    private LocalDateTime createdOn;
+
+    private LocalDateTime lastUpdatedOn;
+
+    private boolean isDeleted;
 
     private Set<Role> roles;
 }
